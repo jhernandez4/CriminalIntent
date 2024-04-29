dac8ed0e5f8231c1a965105641384d5437cb483a
jhernandez4 <jokhernandez120744@gmail.com>
2024-04-29 11:06:19 -0700

    Create listeners for views

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.Date
@@ -30,6 +31,25 @@ class CrimeDetailFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { buttonView, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
     }
 }
