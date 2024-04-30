8d59713370cfb44183797cfde2f7413a5dbbdd38
jhernandez4 <jokhernandez120744@gmail.com>
2024-04-29 19:41:46 -0700

    Create test file for CrimeDetail fragment

---
+++ app/src/androidTest/java/com/bignerdranch/android/criminalintent/CrimeDetailFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class CrimeDetailFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
