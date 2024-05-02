5ef22c1288440d2a573606d0f51603734cf3e616
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-01 22:49:51 -0700

    Bind title and date in ViewHolder

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -2,12 +2,25 @@ package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
 class CrimeHolder(
     val binding: ListItemCrimeBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime){
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener{
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 
 }
 
@@ -24,9 +37,6 @@ class CrimeListAdapter (
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
-        }
+        holder.bind(crime)
     }
 }
